b568ef6a8a9b6082110a3f778b5bf01888bd267a
Onishi Ryosuke <onishi.appcreate@gmail.com>
2021-01-11 14:05:46 +0900

    ログインボタン実装

--- LINELogin/SceneDelegate.swift
+++ LINELogin/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import LineSDK
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -46,7 +47,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        _ = LoginManager.shared.application(.shared, open: URLContexts.first?.url)
+    }
 
 }
 
--- LINELogin/ViewController.swift
+++ LINELogin/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import LineSDK
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Create Login Button.
+        let loginButton = LoginButton()
+        loginButton.delegate = self
+        
+        // Configuration for permissions and presenting.
+        loginButton.permissions = [.profile]
+        loginButton.presentingViewController = self
+        
+        // Add button to view and layout it.
+        view.addSubview(loginButton)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
     }
 
 
